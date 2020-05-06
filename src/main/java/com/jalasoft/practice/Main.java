3158459a64c48f00c28dfdc5373c077472c3b161
Mariela Cruz <heimys>
2020-05-06 11:47:23 -0400

    Adding FILE type parameter for POST request

--- src/main/java/com/jalasoft/practice/Main.java
+++ src/main/java/com/jalasoft/practice/Main.java
@@ -1,10 +1,13 @@
 package com.jalasoft.practice;
 
 import com.jalasoft.practice.controller.HelloController;
+import com.jalasoft.practice.storage.StorageProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
